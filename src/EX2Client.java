2f0ae08cecd8e788695f89ee86603214621c0bd9
Caleb Chong <czchong@cpp.edu>
2017-10-11 15:41:51 -0700

    make compatible with professor's server

--- src/EX2Client.java
+++ src/EX2Client.java
@@ -1,19 +1,29 @@
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.zip.CRC32;
 
 public class EX2Client {
 	public static void main(String[] args ){
-		try (Socket socket = new Socket("localhost", 38102)) {
+		try (Socket socket = new Socket("18.221.102.182", 38102)) {
 			System.out.println("Connected to server.");
 			InputStream is = socket.getInputStream();
+			
 			byte[] msg = new byte[100];
-			byte[] temp = new byte[2];
+			
+			for( int i = 0; i < msg.length ; i++){
+//				if ( i != 0 && i % 10 == 0){
+//					System.out.print("\n");
+//				}
+				int x1 = is.read();
+				int x2 = is.read();
+//				System.out.print(String.format("%02X %02X,", x1, x2));
+				msg[i] = (byte)(x1 * 0x10 + x2 );
+			}
+			
 			System.out.print("Received bytes:");
-			for( int i = 0; i < 100; i++){
-				is.read(temp, 0, 2);
-				msg[i] = (byte)(temp[0] * 0x10 + temp[1]);
+			for( int i = 0; i < msg.length ; i++){
 				if ( i % 10 == 0){
 					System.out.print("\n   ");
 				}
@@ -21,7 +31,7 @@ public class EX2Client {
 			}
 			System.out.println();
 			CRC32 crc = new CRC32();
-			crc.update(msg);
+			crc.update(msg, 0, msg.length);
 			long crcVal = crc.getValue();
 			OutputStream os = socket.getOutputStream();
 			System.out.println("Generated CRC Value: " + Long.toHexString(crcVal).toUpperCase() );
@@ -30,16 +40,32 @@ public class EX2Client {
 				crcBytes[i] = (byte)(crcVal & 0xFF);
 				crcVal = crcVal / 0x100;
 			}
+//			for ( int i = 0; i < 4; i++){
+//				crcBytes[i] = (byte)(crcVal & 0xFF);
+//				crcVal = crcVal / 0x100;
+//			}
+//			System.out.println(Arrays.toString(crcBytes));
+//			for( int i = 0; i < 4 ; i++){
+//				System.out.print(String.format("%02X ", crcBytes[i] ));
+//			}
 			os.write(crcBytes, 0, 4);
-			if ( is.read() == 1 ){
+//			while ( true ) {
+//				System.out.println(is.read());
+//			}
+			
+			int result = is.read();
+			if ( result ==  1 ){
 				System.out.println("Response good.");
 			} else {
 				System.out.println("Response bad.");
 			}
-			socket.close();
-			System.out.println("Disconnected from server.");
+
+//			System.out.println("Disconnected from server.");
+			
         } catch ( Exception e ){
         	e.printStackTrace();
+        } finally {
+        	System.out.println("Disconnected from server.");
         }
 		
 	}
