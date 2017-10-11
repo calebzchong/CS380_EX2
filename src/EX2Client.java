536df50849cd486a93f472d6839a57e0177e4c1f
Caleb Chong <czchong@cpp.edu>
2017-10-11 15:43:27 -0700

    clean up code

--- src/EX2Client.java
+++ src/EX2Client.java
@@ -1,7 +1,6 @@
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Arrays;
 import java.util.zip.CRC32;
 
 public class EX2Client {
@@ -12,16 +11,14 @@ public class EX2Client {
 			
 			byte[] msg = new byte[100];
 			
+			// Read bytes
 			for( int i = 0; i < msg.length ; i++){
-//				if ( i != 0 && i % 10 == 0){
-//					System.out.print("\n");
-//				}
 				int x1 = is.read();
 				int x2 = is.read();
-//				System.out.print(String.format("%02X %02X,", x1, x2));
 				msg[i] = (byte)(x1 * 0x10 + x2 );
 			}
 			
+			// Print bytes
 			System.out.print("Received bytes:");
 			for( int i = 0; i < msg.length ; i++){
 				if ( i % 10 == 0){
@@ -30,38 +27,30 @@ public class EX2Client {
 				System.out.print(String.format("%02X", msg[i]));
 			}
 			System.out.println();
+			
+			// Generate CRC 
 			CRC32 crc = new CRC32();
 			crc.update(msg, 0, msg.length);
 			long crcVal = crc.getValue();
 			OutputStream os = socket.getOutputStream();
 			System.out.println("Generated CRC Value: " + Long.toHexString(crcVal).toUpperCase() );
+			
+			// Split CRC into four, one byte values
 			byte[] crcBytes = new byte[4];
 			for ( int i = 3; i >= 0; i--){
 				crcBytes[i] = (byte)(crcVal & 0xFF);
 				crcVal = crcVal / 0x100;
 			}
-//			for ( int i = 0; i < 4; i++){
-//				crcBytes[i] = (byte)(crcVal & 0xFF);
-//				crcVal = crcVal / 0x100;
-//			}
-//			System.out.println(Arrays.toString(crcBytes));
-//			for( int i = 0; i < 4 ; i++){
-//				System.out.print(String.format("%02X ", crcBytes[i] ));
-//			}
+
 			os.write(crcBytes, 0, 4);
-//			while ( true ) {
-//				System.out.println(is.read());
-//			}
 			
+			// Print results
 			int result = is.read();
 			if ( result ==  1 ){
 				System.out.println("Response good.");
 			} else {
 				System.out.println("Response bad.");
-			}
-
-//			System.out.println("Disconnected from server.");
-			
+			}			
         } catch ( Exception e ){
         	e.printStackTrace();
         } finally {
